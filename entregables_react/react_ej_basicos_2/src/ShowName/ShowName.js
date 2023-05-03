18e877d502d19c41d8c897596858c28f6cfeca88
Jie Hu Zhao <48723634+jiehuzhao99@users.noreply.github.com>
2023-05-03 20:43:10 +0200

    untracked

---
+++ entregables_react/react_ej_basicos_2/src/ShowName/ShowName.js
@@ -0,0 +1,11 @@
+const ShowName = () => {
+    const user = { name: 'John Doe' };
+
+    return ( 
+        <div>
+            <p>{user.name}</p>
+        </div>
+     );
+}
+ 
+export default ShowName;
